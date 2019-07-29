b7c316101973f53290c49c3ad08a7bd30e8daf3f
joemcgirr <46727259+joemcgirr@users.noreply.github.com>
2019-07-29 09:40:11 -0400

    Update quality_control.R

--- workflows/rna_2018/quality_control.R
+++ workflows/rna_2018/quality_control.R
@@ -5,9 +5,9 @@ map <- read.table("C:/Users/jmcgirr/Documents/all_2018_samples/mapping_stats_qc/
 feat_counts <- read.table("C:/Users/jmcgirr/Documents/all_2018_samples/mapping_stats_qc/feature_counts/feature_counts.txt", stringsAsFactors = FALSE, header = TRUE, sep = "\t")
 head(feat_counts)
 
-#mis_indivs <- c("CVT1","CVT2","CVT3","CWT1","CWT2","CWT3","CXT1","CXT2","CXT3","LFT1","LFT2","LFT3","LIT1","LIT2","LIT3","NAT1","NAT2","NAT3","OVT1","OVT2","OVT3","OYT1","OYT2","OYT3","PAT1","PAT2","PAT3","OYE1","OYE2","OYE3","OYE4","OYE5","CAT1","CAT2","CAT3","CPT1","CPT2","NCT1","NCT2","NCT3","OAT1","OAT2","OMT1","OMT2","OMT3","OPT1","OPT2","OPT3","CAE1","CAE2","CAE3","CAE4","CAE5","CPE1","CPE2","CPE3","CPE4","CPE5","NCE1","NCE2","NCE3","NCE4","NCE5","OAE1","OAE2","OAE3","OAE4","OME1","OME2","OME3","OME4","OME5","OPE1","OPE2","OPE3","OPE4","OPE5","CUT1","CUT2","CUT3","LGT1","LGT2","LGT3","LKT1","LKT2","LKT3","OUT1","OUT2","OUT3","OXT1","OXT2","OXT3","OZT1","OZT2","OZT3","CPU1","CPU3","CPU5","CVE1","CVE2","CVE5","CWE2","CWE3","CWE4","CXE2","CXE3","CXE4","LFE2","LFE3","LFE4","LGE3","LGE4","LIE2","LIE3","LIE5","LKE1","LKE2","LKE3","NAE1","NAE2","NAE4","OUE1","OUE3","OUE4","OVE1","OVE4","OVE5","OXE2","OZE2","OZE4","OZE5","PAE1","PAE2","PAE5","CMT1","CMT2","CMT3","CPT3","CQT1","CQT2","OAT3","CME1","CME2","CME5","CQE1","CQE2","CQE3")
+mis_indivs <- c("CVT1","CVT2","CVT3","CWT1","CWT2","CWT3","CXT1","CXT2","CXT3","LFT1","LFT2","LFT3","LIT1","LIT2","LIT3","NAT1","NAT2","NAT3","OVT1","OVT2","OVT3","OYT1","OYT2","OYT3","PAT1","PAT2","PAT3","OYE1","OYE2","OYE3","OYE4","OYE5","CAT1","CAT2","CAT3","CPT1","CPT2","NCT1","NCT2","NCT3","OAT1","OAT2","OMT1","OMT2","OMT3","OPT1","OPT2","OPT3","CAE1","CAE2","CAE3","CAE4","CAE5","CPE1","CPE2","CPE3","CPE4","CPE5","NCE1","NCE2","NCE3","NCE4","NCE5","OAE1","OAE2","OAE3","OAE4","OME1","OME2","OME3","OME4","OME5","OPE1","OPE2","OPE3","OPE4","OPE5","CUT1","CUT2","CUT3","LGT1","LGT2","LGT3","LKT1","LKT2","LKT3","OUT1","OUT2","OUT3","OXT1","OXT2","OXT3","OZT1","OZT2","OZT3","CPU1","CPU3","CPU5","CVE1","CVE2","CVE5","CWE2","CWE3","CWE4","CXE2","CXE3","CXE4","LFE2","LFE3","LFE4","LGE3","LGE4","LIE2","LIE3","LIE5","LKE1","LKE2","LKE3","NAE1","NAE2","NAE4","OUE1","OUE3","OUE4","OVE1","OVE4","OVE5","OXE2","OZE2","OZE4","OZE5","PAE1","PAE2","PAE5","CMT1","CMT2","CMT3","CPT3","CQT1","CQT2","OAT3","CME1","CME2","CME5","CQE1","CQE2","CQE3")
 #no lake crosses
-mis_indivs <-c("CAE1","CAE2","CAE3","CAE4","CAE5","CME1","CME2","CME5","CPE1","CPE2","CPE3","CPE4","CPE5","CQE1","CQE2","CQE3","NCE1","NCE2","NCE3","NCE4","NCE5","OAE1","OAE2","OAE3","OAE4","OME1","OME2","OME3","OME4","OME5","OPE1","OPE2","OPE3","OPE4","OPE5","CPU1","CPU3","CPU5","CVE1","CVE2","CVE5","CWE2","CWE3","CWE4","CXE2","CXE3","CXE4","NAE1","NAE2","NAE4","OUE1","OUE3","OUE4","OVE1","OVE4","OVE5","OXE2","OYE1","OYE2","OYE3","OYE4","OYE5","OZE2","OZE4","OZE5","PAE1","PAE2","PAE5","CAT1","CAT2","CAT3","CMT1","CMT2","CMT3","CPT1","CPT2","CPT3","CQT1","CQT2","NCT1","NCT2","NCT3","OAT1","OAT2","OAT3","OMT1","OMT2","OMT3","OPT1","OPT2","OPT3","CUT1","CUT2","CUT3","CVT1","CVT2","CVT3","CWT1","CWT2","CWT3","CXT1","CXT2","CXT3","NAT1","NAT2","NAT3","OUT1","OUT2","OUT3","OVT1","OVT2","OVT3","OXT1","OXT2","OXT3","OYT1","OYT2","OYT3","OZT1","OZT2","OZT3","PAT1","PAT2","PAT3")
+#mis_indivs <-c("CAE1","CAE2","CAE3","CAE4","CAE5","CME1","CME2","CME5","CPE1","CPE2","CPE3","CPE4","CPE5","CQE1","CQE2","CQE3","NCE1","NCE2","NCE3","NCE4","NCE5","OAE1","OAE2","OAE3","OAE4","OME1","OME2","OME3","OME4","OME5","OPE1","OPE2","OPE3","OPE4","OPE5","CPU1","CPU3","CPU5","CVE1","CVE2","CVE5","CWE2","CWE3","CWE4","CXE2","CXE3","CXE4","NAE1","NAE2","NAE4","OUE1","OUE3","OUE4","OVE1","OVE4","OVE5","OXE2","OYE1","OYE2","OYE3","OYE4","OYE5","OZE2","OZE4","OZE5","PAE1","PAE2","PAE5","CAT1","CAT2","CAT3","CMT1","CMT2","CMT3","CPT1","CPT2","CPT3","CQT1","CQT2","NCT1","NCT2","NCT3","OAT1","OAT2","OAT3","OMT1","OMT2","OMT3","OPT1","OPT2","OPT3","CUT1","CUT2","CUT3","CVT1","CVT2","CVT3","CWT1","CWT2","CWT3","CXT1","CXT2","CXT3","NAT1","NAT2","NAT3","OUT1","OUT2","OUT3","OVT1","OVT2","OVT3","OXT1","OXT2","OXT3","OYT1","OYT2","OYT3","OZT1","OZT2","OZT3","PAT1","PAT2","PAT3")
 
 map <- map[map$alt_name %in% mis_indivs,]
 # number of total reads for misexpression dataset (149 indivs, 147 if PCA outliers removed)
@@ -53,12 +53,13 @@ p1 <- ggboxplot(box_plts, "species_region", "percent_assigned",color = cols, yla
 #tiff("C:/Users/jmcgirr/Documents/all_2018_samples/manuscript_figs/supp/prop_reads_assigned_species.tiff", width = 6, height = 4, units = 'in', res = 1000)
 p1
 #dev.off()
-cols <- c(red,"#000000")
+cols <- c("black","#000000")
 box_plts <- info[order(info$stage),] 
 p1 <- ggboxplot(box_plts, "stage", "percent_assigned",color = cols, ylab = "% reads assigned to features\n", xlab="stage")+
-  stat_compare_means(method = "t.test", label.y = max(info$percent_assigned)+(max(info$percent_assigned)*.05),label.x = 1.25)+      # Add global p-value
-  stat_compare_means(label = "p.signif", method = "t.test",ref.group = ".all.", label.y = (max(info$percent_assigned)+(max(info$percent_assigned)*.01)))
-#tiff("C:/Users/jmcgirr/Documents/all_2018_samples/manuscript_figs/supp/prop_reads_assigned_stage.tiff", width = 4, height = 4, units = 'in', res = 1000)
+  #stat_compare_means(method = "t.test", label.y = max(info$percent_assigned)+(max(info$percent_assigned)*.05),label.x = 1.25)+      # Add global p-value
+  stat_compare_means(label = "p.signif", method = "t.test",ref.group = ".all.", label.y = (max(info$percent_assigned)+(max(info$percent_assigned)*.01)))+
+  scale_x_discrete(labels = c("2dpf", "8dpf"))
+#tiff("C:/Users/jmcgirr/Documents/all_2018_samples/manuscript_figs/supp/qc/prop_reads_assigned_stage.tiff", width = 4, height = 4, units = 'in', res = 1000)
 p1
 #dev.off()
 
@@ -139,70 +140,72 @@ median(info$avg_depth_across_features)
 #write.table(info, "C:/Users/jmcgirr/Documents/all_2018_samples/mapping_stats_qc/all_qc_stats.txt", quote = FALSE, row.names = FALSE, sep = "\t")
 
 cols <- c(red,"darkgrey",pur,gre,"black",dkr,blu)
+cols <- c(rep("black",7))
 unique(info$species_region_lake_cross_with_species)
 #info <- info[which(info$stage == "8dpf"),]
 box_plts <- info[order(info$species_region_lake_cross_with_species),] 
 
 p1 <- ggboxplot(box_plts, "species_region_lake_cross_with_species", "total_norm_cts",color = cols, ylab = "total normalized counts\n", xlab="")+
-  stat_compare_means(method = "anova", label.y = max(info$total_norm_cts)+(max(info$total_norm_cts)*.05),label.x = 2.25)+      # Add global p-value
-  scale_x_discrete(labels= c("generalists","new\nprovidence", "species\nhybrids","molluscivores", "north \ncarolina","outgroup\ngeneralist\nhybrids", "scale-eaters"))+
+  #stat_compare_means(method = "anova", label.y = max(info$total_norm_cts)+(max(info$total_norm_cts)*.05),label.x = 2.25)+      # Add global p-value
+  scale_x_discrete(labels= c("generalists","new\nprovidence", "san salvador\nF1 hybrids","molluscivores", "north \ncarolina","outgroup\ngeneralist\nF1 hybrids", "scale-eaters"))+
   theme(axis.text=element_text(size=8.5))
   #stat_compare_means(label = "p.signif", method = "t.test",ref.group = ".all.", label.y = (max(info$total_norm_cts)+(max(info$total_norm_cts)*.01)))
 #an <- aov(info$total_norm_cts~info$species_region_lake_cross_with_species)
 #summary(an)
-#tiff("C:/Users/jmcgirr/Documents/all_2018_samples/manuscript_figs/supp/qc_norm_cts.tiff", width = 6, height = 4, units = 'in', res = 1000)
+#tiff("C:/Users/jmcgirr/Documents/all_2018_samples/manuscript_figs/supp/qc/qc_norm_cts.tiff", width = 6.5, height = 3, units = 'in', res = 1000)
 p1
 #dev.off()
 p1 <- ggboxplot(box_plts, "species_region_lake_cross_with_species", "percent_assigned",color = cols, ylab = "% reads assigned to features\n", xlab="")+
-  stat_compare_means(method = "anova", label.y = max(info$percent_assigned)+(max(info$percent_assigned)*.01),label.x = 2.25)+      # Add global p-value
-  scale_x_discrete(labels= c("generalists","new\nprovidence", "species\nhybrids","molluscivores", "north \ncarolina","outgroup\ngeneralist\nhybrids", "scale-eaters"))+
+  #stat_compare_means(method = "anova", label.y = max(info$percent_assigned)+(max(info$percent_assigned)*.01),label.x = 2.25)+      # Add global p-value
+  scale_x_discrete(labels= c("generalists","new\nprovidence", "san salvador\nF1 hybrids","molluscivores", "north \ncarolina","outgroup\ngeneralist\nF1 hybrids", "scale-eaters"))+
   theme(axis.text=element_text(size=8.5))
 #stat_compare_means(label = "p.signif", method = "t.test",ref.group = ".all.", label.y = (max(info$percent_assigned)+(max(info$percent_assigned)*.01)), hide.ns = TRUE)
 #an <- aov(info$percent_assigned~info$species_region_lake_cross_with_species)
 #summary(an)
-#tiff("C:/Users/jmcgirr/Documents/all_2018_samples/manuscript_figs/supp/prop_reads_assigned_species.tiff", width = 6, height = 4, units = 'in', res = 1000)
+#tiff("C:/Users/jmcgirr/Documents/all_2018_samples/manuscript_figs/supp/qc/prop_reads_assigned_species.tiff", width = 6.5, height = 3, units = 'in', res = 1000)
 p1
 #dev.off()
 p1 <- ggboxplot(box_plts, "species_region_lake_cross_with_species", "median_gc_content",color = cols, ylab = "median % GC content\n", xlab="")+
-  stat_compare_means(method = "anova", label.y = max(info$median_gc_content)+(max(info$median_gc_content)*.01),label.x = 2.25)+      # Add global p-value
-  scale_x_discrete(labels= c("generalists","new\nprovidence", "species\nhybrids","molluscivores", "north \ncarolina","outgroup\ngeneralist\nhybrids", "scale-eaters"))+
+  #stat_compare_means(method = "anova", label.y = max(info$median_gc_content)+(max(info$median_gc_content)*.01),label.x = 2.25)+      # Add global p-value
+  scale_x_discrete(labels= c("generalists","new\nprovidence", "san salvador\nF1 hybrids","molluscivores", "north \ncarolina","outgroup\ngeneralist\nF1 hybrids", "scale-eaters"))+
   theme(axis.text=element_text(size=8.5))
 #stat_compare_means(label = "p.signif", method = "t.test",ref.group = ".all.", label.y = (max(info$median_gc_content)+(max(info$median_gc_content)*.01)), hide.ns = TRUE)
 #an <- aov(info$median_gc_content~info$species_region_lake_cross_with_species)
 #summary(an)
-#tiff("C:/Users/jmcgirr/Documents/all_2018_samples/manuscript_figs/supp/qc_gc_content.tiff", width = 6, height = 4, units = 'in', res = 1000)
+#tiff("C:/Users/jmcgirr/Documents/all_2018_samples/manuscript_figs/supp/qc/qc_gc_content.tiff", width =6.5, height = 3, units = 'in', res = 1000)
 p1
 #dev.off()
 p1 <- ggboxplot(box_plts, "species_region_lake_cross_with_species", "avg_depth_across_features",color = cols, ylab = "mean depth across features\n", xlab="")+
-  stat_compare_means(method = "anova", label.y = max(info$avg_depth_across_features)+(max(info$avg_depth_across_features)*.05),label.x = 2.25)+      # Add global p-value
-  scale_x_discrete(labels= c("generalists","new\nprovidence", "species\nhybrids","molluscivores", "north \ncarolina","outgroup\ngeneralist\nhybrids", "scale-eaters"))+
-  theme(axis.text=element_text(size=8.5))
+  #stat_compare_means(method = "anova", label.y = max(info$avg_depth_across_features)+(max(info$avg_depth_across_features)*.05),label.x = 2.25)+      # Add global p-value
+  scale_x_discrete(labels= c("generalists","new\nprovidence", "san salvador\nF1 hybrids","molluscivores", "north \ncarolina","outgroup\ngeneralist\nF1 hybrids", "scale-eaters"))+
+  theme(axis.text=element_text(size=8.5))+
+  scale_y_continuous(limits = c(0, 2000))
 #stat_compare_means(label = "p.signif", method = "t.test",ref.group = ".all.", label.y = (max(info$avg_depth_across_features)+(max(info$avg_depth_across_features)*.01)))
 #an <- aov(info$avg_depth_across_features~info$species_region_lake_cross_with_species)
 #summary(an)
-#tiff("C:/Users/jmcgirr/Documents/all_2018_samples/manuscript_figs/supp/qc_depth.tiff", width = 6, height = 4, units = 'in', res = 1000)
+#tiff("C:/Users/jmcgirr/Documents/all_2018_samples/manuscript_figs/supp/qc/qc_depth.tiff", width = 6.5, height = 3, units = 'in', res = 1000)
 p1
 #dev.off()
 p1 <- ggboxplot(box_plts, "species_region_lake_cross_with_species", "tin_median",color = cols, ylab = "median TIN\n", xlab="")+
-  stat_compare_means(method = "anova", label.y = max(info$tin_median)+(max(info$tin_median)*.05),label.x = 2.25)+      # Add global p-value
-  scale_x_discrete(labels= c("generalists","new\nprovidence", "species\nhybrids","molluscivores", "north \ncarolina","outgroup\ngeneralist\nhybrids", "scale-eaters"))+
+  #stat_compare_means(method = "anova", label.y = max(info$tin_median)+(max(info$tin_median)*.05),label.x = 2.25)+      # Add global p-value
+  scale_x_discrete(labels= c("generalists","new\nprovidence", "san salvador\nF1 hybrids","molluscivores", "north \ncarolina","outgroup\ngeneralist\nF1 hybrids", "scale-eaters"))+
   theme(axis.text=element_text(size=8.5))+
   stat_compare_means(label = "p.signif", method = "t.test",ref.group = ".all.", label.y = (max(info$tin_median)+(max(info$tin_median)*.01)))
-#an <- aov(info$tin_median~info$species_region_lake_cross_with_species)
+an <- aov(info$tin_median~info$species_region_lake_cross_with_species)
 TukeyHSD(an)
 #summary(an)
-#tiff("C:/Users/jmcgirr/Documents/all_2018_samples/manuscript_figs/supp/qc_tin.tiff", width = 6, height = 4, units = 'in', res = 1000)
+#tiff("C:/Users/jmcgirr/Documents/all_2018_samples/manuscript_figs/supp/qc/qc_tin.tiff", width = 6.5, height = 3, units = 'in', res = 1000)
 p1
 #dev.off()
 p1 <- ggboxplot(box_plts, "species_region_lake_cross_with_species", "prop_duplicate_reads",color = cols, ylab = "prop_duplicate_reads\n", xlab="species")+
   scale_y_continuous(labels = scales::scientific)+
   stat_compare_means(method = "anova", label.y = max(info$prop_duplicate_reads)+(max(info$prop_duplicate_reads)*.05),label.x = 2.25)+      # Add global p-value
-  scale_x_discrete(labels= c("generalists","new\nprovidence", "species\nhybrids","molluscivores", "north \ncarolina","outgroup\ngeneralist\nhybrids", "scale-eaters"))+
+  scale_x_discrete(labels= c("generalists","new\nprovidence", "san salvador\nF1 hybrids","molluscivores", "north \ncarolina","outgroup\ngeneralist\nF1 hybrids", "scale-eaters"))+
   theme(axis.text=element_text(size=8.5))+
   stat_compare_means(label = "p.signif", method = "t.test",ref.group = ".all.", label.y = (max(info$prop_duplicate_reads)+(max(info$prop_duplicate_reads)*.01)))
 #an <- aov(info$tin_median~info$species_region_lake_cross_with_species)
 #summary(an)
-#tiff("C:/Users/jmcgirr/Documents/all_2018_samples/manuscript_figs/supp/qc_dups.tiff", width = 6, height = 4, units = 'in', res = 1000)
+#tiff("C:/Users/jmcgirr/Documents/all_2018_samples/manuscript_figs/supp/qc/qc_dups.tiff", width = 6.5, height = 3, units = 'in', res = 1000)
 p1
 #dev.off()
 
